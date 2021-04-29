ab89df429c100ff9cbbe229d05ca8f7462dd158e
Jan Everaert <jan@crashlab.be>
2021-04-29 09:43:27 +0200

    rem file

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 "use strict";
 
-import List from "./List.js";
 import User from "./User.js";
 
 
 const service = {
   userID: null,
-  list: [],
   user: new User(),
   init() {
     this.user.initFields();
 
     if(this.user.token !== null) {
-      this.list = new List(this.user.token);
+      // fetch it!
     } 
   }
 
